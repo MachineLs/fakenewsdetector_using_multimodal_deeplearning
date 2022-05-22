259455de9cfb98b4657cd43f55fc0301dd45408d
Lunshu Sun <lunshusun@gmail.com>
2022-05-22 12:06:09 -0400

    add loss functions

--- detector/model.py
+++ detector/model.py
@@ -1,6 +1,9 @@
 import torch
 import torch.nn.functional as F
 from torch import nn
+from keras import backend as K
+import tensorflow as tf
+import numpy as np
 
 from image import ImageTransformerEncoder, ImageResnetEncoder
 from text import TextEncoder
@@ -119,3 +122,60 @@ def cross_entropy(preds, targets, reduction='none'):
         return loss
     elif reduction == "mean":
         return loss.mean()
+
+################### our method
+def mean_square_error(y_true, y_pred):
+    return K.mean(K.abs(y_true-y_pred), axis=-1)
+
+
+def Hinge(yHat, y):
+    #print(yHat, y)
+    return np.max([0., 1. - yHat * y])
+
+
+################################################
+def cross_entropy(y_true, y_pred):
+    return K.categorical_crossentropy(y_true, y_pred)
+
+def symmetric_cross_entropy(alpha, beta):
+    def loss(y_true, y_pred):
+        y_true_1 = y_true
+        y_pred_1 = y_pred
+
+        y_true_2 = y_true
+        y_pred_2 = y_pred
+
+        y_pred_1 = tf.clip_by_value(y_pred_1, 1e-7, 1.0)
+        y_true_2 = tf.clip_by_value(y_true_2, 1e-4, 1.0)
+
+        return alpha*tf.reduce_mean(-tf.reduce_sum(y_true_1 * tf.log(y_pred_1), axis = -1)) + beta*tf.reduce_mean(-tf.reduce_sum(y_pred_2 * tf.log(y_true_2), axis = -1))
+    return loss
+
+
+def lsr(y_true, y_pred):
+    epsilon = 0.1
+    y_smoothed_true = y_true * (1 - epsilon - epsilon / 10.0)
+    y_smoothed_true = y_smoothed_true + epsilon / 10.0
+
+    y_pred_1 = tf.clip_by_value(y_pred, 1e-7, 1.0)
+
+    return tf.reduce_mean(-tf.reduce_sum(y_smoothed_true * tf.log(y_pred_1), axis=-1))
+
+def generalized_cross_entropy(y_true, y_pred):
+    """
+    2018 - nips - Generalized Cross Entropy Loss for Training Deep Neural Networks with Noisy Labels.
+    """
+    q = 0.7
+    t_loss = (1 - tf.pow(tf.reduce_sum(y_true * y_pred, axis=-1), q)) / q
+    return tf.reduce_mean(t_loss)
+
+def joint_optimization_loss(y_true, y_pred):
+    """
+    2018 - cvpr - Joint optimization framework for learning with noisy labels.
+    """
+    y_pred_avg = K.mean(y_pred, axis=0)
+    p = np.ones(10, dtype=np.float32) / 10.
+    l_p = - K.sum(K.log(y_pred_avg) * p)
+    l_e = K.categorical_crossentropy(y_pred, y_pred)
+    return K.categorical_crossentropy(y_true, y_pred) + 1.2 * l_p + 0.8 * l_e
+
